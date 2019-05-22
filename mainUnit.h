7a66c9b9a732520f8ce01c70269eb5e58072a674
wojtex77 <37085911+wojtex77@users.noreply.github.com>
2019-05-22 21:43:54 +0200

    initial-commit

---
+++ mainUnit.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef mainUnitH
+#define mainUnitH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TMainForm : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+        __fastcall TMainForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TMainForm *MainForm;
+//---------------------------------------------------------------------------
+#endif
