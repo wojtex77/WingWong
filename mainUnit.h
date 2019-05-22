c3b0fc044a3c6c90c9cc802cd7580157278bd708
wojtex77 <37085911+wojtex77@users.noreply.github.com>
2019-05-22 22:44:38 +0200

    table-backgroung-added

--- mainUnit.h
+++ mainUnit.h
@@ -7,10 +7,13 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TMainForm : public TForm
 {
 __published:	// IDE-managed Components
+        TImage *Image1;
 private:	// User declarations
 public:		// User declarations
         __fastcall TMainForm(TComponent* Owner);
